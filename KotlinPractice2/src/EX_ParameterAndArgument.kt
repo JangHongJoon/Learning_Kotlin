f1aae2510293b029ad60d875f50c8d6b32ff8693
JangHongJoon <37648375+JangHongJoon@users.noreply.github.com>
2021-03-20 21:14:22 +0900

    20210320

---
+++ KotlinPractice2/src/EX_ParameterAndArgument.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>):Unit{
+    println(cToF(30))
+    println(getAverage(89,96))
+}
+
+fun cToF(celsius: Int):Double = celsius*1.8+32
+fun getAverage(a: Int, b: Int):Double = (a+b)/2.0
